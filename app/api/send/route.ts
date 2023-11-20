1e43ddcf8674202d70d157fd30691ab83d789237
samin <84340577+HGSChandeepa@users.noreply.github.com>
2023-11-21 02:00:17 +0530

    setting up pdf

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+import PDFDocument from "pdfkit";
 
 export async function POST(request: any) {
   try {
@@ -14,6 +15,13 @@ export async function POST(request: any) {
         user: "saminchandeepa@gmail.com",
         pass: process.env.NEXT_PUBLIC_PASSWORD,
       },
+      attachments: [
+        {
+          filename: "project.pdf",
+          path: "C:/Users/Samin Chandeepa/Desktop/Project.pdf",
+          contentType: "application/pdf",
+        },
+      ],
     });
 
     const mailOption = {
@@ -55,6 +63,72 @@ export async function POST(request: any) {
       { message: "Email Sent Successfully" },
       { status: 200 }
     );
+
+    // --------------
+
+    // const generatePDFContent = () => {
+    //   return new Promise((resolve) => {
+    //     const doc = new PDFDocument();
+    //     const chunks: any = [];
+
+    //     // Add content to the PDF
+    //     doc.fontSize(16).text("Hello, this is a generated PDF!", 100, 100);
+
+    //     // Capture PDF chunks as buffers
+    //     doc.on("data", (chunk: any) => {
+    //       chunks.push(chunk);
+    //     });
+
+    //     // When the PDF is finished, resolve the promise with the concatenated buffer
+    //     doc.on("end", () => {
+    //       const pdfBuffer = Buffer.concat(chunks);
+    //       resolve(pdfBuffer);
+    //     });
+
+    //     // Finalize the PDF
+    //     doc.end();
+    //   });
+    // };
+
+    // const sendPDFByEmail = async (pdfContent: any) => {
+    //   // Create a nodemailer transporter
+    //   const transporter = nodemailer.createTransport({
+    //     service: "gmail",
+    //     auth: {
+    //       user: "saminchandeepa@gmail.com",
+    //       pass: process.env.NEXT_PUBLIC_PASSWORD,
+    //     },
+    //   });
+
+    //   // Define email options
+    //   const mailOptions = {
+    //     from: "saminchandeepa@gmail.com",
+    //     to: "adomicarts@gmail.com",
+    //     subject: "Generated PDF",
+    //     text: "Attached is the generated PDF.",
+    //     attachments: [
+    //       {
+    //         filename: "generated-pdf.pdf",
+    //         content: pdfContent,
+    //         encoding: "base64",
+    //       },
+    //     ],
+    //   };
+
+    //   // Send the email
+    //   await transporter.sendMail(mailOptions);
+    // };
+
+    // const pdfContent = await generatePDFContent();
+    // console.log("pdfContent");
+
+    // // Send the PDF via email
+    // await sendPDFByEmail(pdfContent);
+
+    // return NextResponse.json(
+    //   { message: "Email Sent Successfully" },
+    //   { status: 200 }
+    // );
   } catch (error) {
     console.error("Error sending email:", error);
 
